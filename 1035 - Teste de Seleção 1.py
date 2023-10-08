1acfe0a90227152c3a0e86cbf264751098debe39
adryanmaikel <adryanmaikel@gmail.com>
2023-10-07 21:06:43 -0300

    finalizando 1035

---
+++ 1035 - Teste de Seleção 1.py
@@ -0,0 +1,5 @@
+a, b, c, d = [int(i) for i in input().split()]
+if(b > c and d > a and c + d > a + b and c > 0 and d > 0 and a % 2 == 0):
+  print("Valores aceitos")
+else:
+  print("Valores nao aceitos")
