37421c427a03b8f723ce92b5e92a72fbc4104238
AdryanMaikel <adryanmaikel@gmail.com>
2023-10-30 20:00:22 -0300

    imprimindo de 1 até 10000 numeros divisiveis por N

--- 1075 - Resto 2.py
+++ 1075 - Resto 2.py
@@ -1,3 +1,14 @@
+"""
+Leia um valor inteiro N. Apresente todos os números entre 1 e 10000
+que divididos por N dão resto igual a 2.
+
+Entrada
+A entrada contém um valor inteiro N (N < 10000).
+
+Saída
+Imprima todos valores que quando divididos por N dão resto = 2, um
+por linha.
+"""
 divisor = int(input())
 for number in range(1, 10000):
   print(number) if number % divisor == 2 else 0
