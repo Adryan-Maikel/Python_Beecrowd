61bbcc21d2f768cc455019ac91712266598a6ee3
AdryanMaikel <adryanmaikel@gmail.com>
2023-10-30 20:00:53 -0300

    calculando uma media ponderada

---
+++ 1079 - Médias Ponderadas.py
@@ -0,0 +1,21 @@
+"""
+Leia 1 valor inteiro N, que representa o número de casos de teste
+que vem a seguir. Cada caso de teste consiste de 3 valores reais,
+cada um deles com uma casa decimal. Apresente a média ponderada 
+para cada um destes conjuntos de 3 valores, sendo que o primeiro
+valor tem peso 2, o segundo valor tem peso 3 e o terceiro valor 
+tem peso 5.
+
+Entrada
+O arquivo de entrada contém um valor inteiro N na primeira linha.
+Cada N linha a seguir contém um caso de teste com três valores com
+uma casa decimal cada valor.
+
+Saída
+Para cada caso de teste, imprima a média ponderada dos 3 valores,
+conforme exemplo abaixo.
+"""
+for range in range(int(input())):
+  numbers = [float(number) for number in input().split(' ')]
+  result = (numbers[0] * 2 + numbers[1] * 3 + numbers[2] * 5) / 10
+  print(f'{round(result, 1)}')
