dfc55b15824b3b6fbc47c5ed93d51ae23ede22b6
AdryanMaikel <adryanmaikel@gmail.com>
2023-10-30 16:58:03 -0300

    trocando a biblioteca datetime por while

--- 1061 - Tempo de um Evento.py
+++ 1061 - Tempo de um Evento.py
@@ -25,6 +25,56 @@ Z segundo(s)
 Obs: Considere que o evento do caso de teste para o problema tem duração mínima
 de 1 minuto.
 """
+# -*- coding: utf-8 -*-
+
+day = int(input().split(' ')[1]) # entrada -> Dia 5
+hour, minute, second = [int(i) for i in input().split(' : ')]# 08 : 12 : 23
+day_end = int(input().split(' ')[1]) # entrada -> Dia 9        ||   ||   ||
+hour_end, minute_end, second_end = [int(i) for i in input().split(' : ')]
+diference_days, diference_hour, diference_minute, diference_second = 0, 0, 0, 0
+
+while second != second_end:
+  second += 1
+  diference_second += 1
+  if second == 60:
+    minute += 1
+    second = 0
+  if diference_second == 60:
+    diference_minute += 1
+    diference_second = 0
+    
+while minute != minute_end:
+  minute += 1
+  diference_minute += 1
+  if minute == 60:
+    hour += 1
+    minute = 0
+  if diference_minute == 60:
+    diference_hour += 1
+    diference_minute = 0
+
+while hour != hour_end:
+  hour += 1
+  diference_hour += 1
+  if hour == 24:
+    day += 1
+    hour = 0
+  if diference_minute == 60:
+    diference_hour += 1
+    diference_minute = 0
+
+while day != day_end:
+  diference_days += 1
+  day += 1
+
+print(
+  f'{diference_days} dia(s)\n'
+  f'{diference_hour} hora(s)\n'
+  f'{diference_minute} minuto(s)\n'
+  f'{diference_second} segundo(s)'
+)
+
+""" Segunda solução
 from datetime import datetime
 
 year, month = 2023, 4
@@ -36,12 +86,14 @@ hour_end, minute_end, second_end = [int(i) for i in input().split(' : ')]
 start_date = datetime(year,  month, day, hour, minute, second)
 end_date = datetime(year, month, day_end, hour_end, minute_end, second_end)
 diference = end_date - start_date
-diference_hour = str(diference).split(", ")[1].split(":")[0]
-diference_minute = str(diference).split(", ")[1].split(":")[1]
-diference_second = str(diference).split(", ")[1].split(":")[2]
+diference_hour = str(end_date - start_date).split(", ")[1].split(":")[0]
+diference_minute = str(end_date - start_date).split(", ")[1].split(":")[1]
+diference_second = str(end_date - start_date).split(", ")[1].split(":")[2]
+
 print(
   f'{diference.days} dia(s)\n'
-  f'{diference_hour} hora(s)\n'
-  f'{diference_minute} minuto(s)\n'
-  f'{diference_second} segundo(s)'
+  f'{int(diference_hour)} hora(s)\n'
+  f'{int(diference_minute)} minuto(s)\n'
+  f'{int(diference_second)} segundo(s)'
 )
+"""
