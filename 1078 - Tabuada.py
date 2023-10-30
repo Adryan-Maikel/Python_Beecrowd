0d5613da62d770d466fcd007161f4b75d301115a
AdryanMaikel <adryanmaikel@gmail.com>
2023-10-30 20:00:35 -0300

    criando uma tabuada

---
+++ 1078 - Tabuada.py
@@ -0,0 +1,13 @@
+"""
+Leia 1 valor inteiro N (2 < N < 1000). A seguir, mostre a tabuada de N:      
+1 x N = N      2 x N = 2N        ...       10 x N = 10N
+
+Entrada
+A entrada contém um valor inteiro N (2 < N < 1000).
+
+Saída
+Imprima a tabuada de N, conforme o exemplo fornecido.
+"""
+number = int(input())
+for i in range(1, 11):
+  print(f'{i} x {number} = {i * number}')
